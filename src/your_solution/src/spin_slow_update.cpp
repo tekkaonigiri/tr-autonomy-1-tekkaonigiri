8dbd7d0b5c3451634124f40dbdeae4767e2f44f9
Nicole Fong <nicolefongjw@gmail.com>
2025-10-30 21:04:05 +0000

    Add working Part 1 solution and rebuild repo bc somehow i broke it umm

--- src/your_solution/src/spin_slow_update.cpp
+++ src/your_solution/src/spin_slow_update.cpp
@@ -7,9 +7,31 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+// constructor -- anything inside here runs when nodes starts (add publisher + subscriber)
 SlowSolution::SlowSolution() : Node("slowsolution") {
-  RCLCPP_INFO(this->get_logger(), "Remove this statement from spin_slow_update.cpp");
-  // your code here
+  // publisher
+  publisher_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("predictedpos", 10);
+  
+  // subscriber
+  subscription_ = this->create_subscription<std_msgs::msg::Float64MultiArray>("/measuredpos", 10, std::bind(&SlowSolution::callback_function, this, std::placeholders::_1)
+);
 }
 
-// your code here
+void SlowSolution::callback_function(const std_msgs::msg::Float64MultiArray::SharedPtr msg) {
+    // callback function
+    // extract x and y coordinates -- read x and y from incoming msg
+    double x = msg->data[0];
+    double y = msg->data[1];
+
+    // create new msg for float64multiarray
+    auto predicted_msg = std::make_shared<std_msgs::msg::Float64MultiArray>();
+    
+    // assign + set predicted msg values
+    predicted_msg->data.push_back(x);
+    predicted_msg->data.push_back(y);
+
+    publisher_->publish(*predicted_msg);
+
+    RCLCPP_INFO(this->get_logger(), "Republish x: %f, y: %f", x, y);
+    // debugger print statement
+}
--- src/your_solution/src/spin_slow_update.h
+++ src/your_solution/src/spin_slow_update.h
@@ -9,7 +9,10 @@ class SlowSolution : public rclcpp::Node {
  public:
   SlowSolution();
  private:
-  // your code here
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr publisher_;
+  rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr subscription_;
+
+void callback_function(const std_msgs::msg::Float64MultiArray::SharedPtr msg);
 };
 
 #endif //YOUR_SOLUTION_SRC_SPIN_SLOW_UPDATE_H_
